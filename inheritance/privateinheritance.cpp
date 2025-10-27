234e55145dad0e6868613aef22fea7935d5e40c7
sashipuri <sashipuri@ioepc.edu.np>
2025-10-27 17:30:25 +0545

    privateinheritance

---
+++ inheritance/privateinheritance.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class base
+{
+protected:
+int l,b;
+};
+class derived: private base
+{
+    private:
+     int a,p;
+    public:
+    void input()
+    {
+        cout<<"Enter the value of length and breadth of rectangel"<<endl;
+        cin>>l>>b;
+
+    }
+    void calc_ap()
+    {
+        a = l*b;
+        p = 2*(l+b);
+    }
+
+    void display()
+    {
+        cout<<"The area is = "<<a<<endl<<"The perimeter is= "<<p<<endl;
+    }
+};
+
+ int main() 
+{
+  
+  derived d;
+  d.input();
+  d.calc_ap();
+  d.display();
+  
+return 0;
+}

68890935a1ea7097d1f18c599068902b7703d266
sashipuri <sashipuri@ioepc.edu.np>
2025-10-27 17:45:26 +0545

    multipleinheritance

---
+++ inheritance/multipleinheritance.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+class length
+{
+protected:
+    int l;
+public:
+ void input()
+ {
+    cout<<"Enter the value of length"<<endl;
+    cin>>l;
+ }
+};
+
+class breadth
+{
+    protected:
+        int b;
+    public:
+        void input1()
+        {
+            cout<<"Enter the value of breadth of rectangle"<<endl;
+            cin>>b;
+        }
+};
+class rectangle: public length, public breadth
+{
+    private:
+        int a,p;
+    public:
+        void calc_ap()
+        {
+            a = l*b;
+            p = 2*(l+b);
+
+        } 
+        void display()
+        {
+            cout<<"The area is="<<a<<endl<<"The perimeter is="<<p<<endl;
+        }
+
+};
+ int main() 
+{
+  rectangle r;
+  r.input();
+  r.input1();
+  r.calc_ap();
+  r.display();
+  
+  
+return 0;
+}
